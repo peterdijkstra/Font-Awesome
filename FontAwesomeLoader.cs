076a2c5805be18728083d89c6ac5f1936c10c4a4
peter <peter@crossup.tech>
2020-04-14 10:57:32 +0200

    More unity files

---
+++ FontAwesomeLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public static class FontAwesomeLoader
+{
+	private const string Path = "Packages/com.fontawesome.upm/Styles.uss";
+	
+	public static void ApplySheetTo(VisualElement element)
+	{
+		var sheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(Path);
+		element.styleSheets.Add(sheet);
+	}
+}
